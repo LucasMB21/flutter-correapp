3263ca5620ccf5cac21d302087ed845a4e3e964c
lucas.r.c2010 <lucas.r.c2010@hotmail.com>
2025-04-17 04:56:03 +0000

    fix: correção de imports e exibição de restaurantes na HomeScreen

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/data/categories_data.dart';
 import 'package:myapp/data/restaurant_data.dart';
+import 'package:myapp/model/restaurant.dart';
 import 'package:myapp/ui/home/widgets/category_widget.dart';
+import 'package:myapp/ui/home/widgets/restaurant_widget.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
