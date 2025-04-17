bc7266349b37baf724592055cc9312b603ec2a7e
lucas.r.c2010 <lucas.r.c2010@hotmail.com>
2025-04-17 04:52:12 +0000

    update

--- lib/data/restaurant_data.dart
+++ lib/data/restaurant_data.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
+import 'dart:ffi';
 import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import '../model/restaurant.dart';
 
-class RestaurantData {
-  Future<List<Restaurant>> getRestaurant() async {
-    List<Restaurant> listRestaurant = [];
+class RestaurantData extends ChangeNotifier {
+  List<Restaurant> listRestaurant = [];
 
+  Future<void> getRestaurant() async {
     String jsonString = await rootBundle.loadString('assets/data.json');
     Map<String, dynamic> data = json.decode(jsonString);
     List<dynamic> restaurantsData = data['restaurants'];
@@ -14,6 +16,5 @@ class RestaurantData {
       Restaurant restaurant = Restaurant.fromMap(restaurantData);
       listRestaurant.add(restaurant);
     }
-    return listRestaurant;
   }
 }
